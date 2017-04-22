1cb4c5fe0e0e22a022f38a72c4b0cdecc481481e
Jacob Fink <jacobfink@gmail.com>
2017-04-22 13:18:07 -0500

    Added DateStart and IntervalByHour Columns to TasksToDo Table

---
+++ WsidnData/TasksCompleted.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TasksCompleted]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [DateCreated] DATETIME NOT NULL , 
+    [GoalDescription] NCHAR(200) NOT NULL, 
+    [Category] INT NULL FOREIGN KEY (Category) REFERENCES Categories(Id), 
+    [DateCompleted] DATETIME NOT NULL, 
+)
