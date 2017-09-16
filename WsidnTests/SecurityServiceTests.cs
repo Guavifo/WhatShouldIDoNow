cb5cf20204bfd5e2c763a824ac0f281f7ee45bb1
quakkels <quakkels@gmail.com>
2017-09-16 17:16:17 -0500

    Made login use check for correct hash.

---
+++ WsidnTests/SecurityServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhatShouldIDoNow.DataAccess;
+using WhatShouldIDoNow.Services;
+using Microsoft.AspNetCore.Http;
+
+namespace WsidnTests
+{
+    [TestClass]
+    public class SecurityServiceTests
+    {
+        private Mock<IHttpContextAccessor> _httpContext;
+        private SecurityService _securityService;
+        private Mock<IUserQueries> _userQueryMock;
+ 
+        [TestInitialize]
+        public void Intialize()
+        {
+            _httpContext = new Mock<IHttpContextAccessor>();
+
+            // used http://bcrypthashgenerator.apphb.com/?PlainText=password to get hash
+            _userQueryMock = new Mock<IUserQueries>();
+            _userQueryMock
+                .Setup(x => x.GetPasswordHashByUserName("username"))
+                .Returns("$2a$06$fI/3zuLAmqdzUQ7IpxocOeZMFMimLZitlCwZKXdsm7srSA2biAebi");
+
+            _securityService = new SecurityService(_httpContext.Object, _userQueryMock.Object);
+        }
+
+        [TestMethod]
+        public void PasswordMatchesHash()
+        {
+            // arrange
+            var password = "password";
+            var username = "username";
+
+            // act
+            var result = _securityService.VerifyUserPassword(username, password);
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void PasswordDoesNotMatchHash()
+        {
+            // arrange
+            var password = "wrongpassword";
+            var username = "username";
+
+            // act
+            var result = _securityService.VerifyUserPassword(username, password);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
